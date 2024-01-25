953963f2a77decc914a89675dbe2b4236b6139ae
dgonzalez777 <1989.daniela@gmail.com>
2024-01-24 22:54:44 -0400

    Reorder code useSearch

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,9 @@ import { useState, useRef, useEffect } from "react";
 import Searcher from "./components/Searcher";
 import "./App.css";
 import { Movies } from "./components/Movies";
-import { useMovies } from "./hooks/useMovies";
+import { useMovies, useSearch } from "./hooks/useMovies";
 
 function App() {
-  function useSearch() {
-    //para sacar la logica del componente
-    const isFirstRender = useRef(true);
-    const [search, updateSearch] = useState("");
-    const [error, setError] = useState(null);
-    useEffect(() => {
-      if (isFirstRender.current) {
-        isFirstRender.current = search === "";
-        return;
-      }
-      if (search === "") {
-        setError(" No se puede buscar una película vacía");
-        return;
-      }
-      if (search.match(/^\d+$/)) {
-        setError("No se puede buscar una película con sólo números");
-        return;
-      }
-      if (search.length < 3) {
-        setError("La búsqueda debe tener al menos 3 caracteres");
-        return;
-      }
-      setError(null);
-    }, [search]);
-    return { search, updateSearch, error };
-  }
-
   const { search, updateSearch, error } = useSearch(); // custome Hook
 
   const { movies, loading, getMovies } = useMovies(search);
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,6 +1,33 @@
 import { useState, useEffect, useRef } from "react";
 import { searchMovies } from "../services/movies";
 
+export function useSearch() {
+  //para sacar la logica del componente
+  const isFirstRender = useRef(true);
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError(" No se puede buscar una película vacía");
+      return;
+    }
+    if (search.match(/^\d+$/)) {
+      setError("No se puede buscar una película con sólo números");
+      return;
+    }
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+    setError(null);
+  }, [search]);
+  return { search, updateSearch, error };
+}
+
 export function useMovies(search) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
